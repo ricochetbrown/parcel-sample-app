35b0ab14cb0d094ab6299854c485b453fc8b9d16
Doug Chamberlain <d.chamberlain@nextech.com>
2019-03-19 13:28:04 -0400

    updated to the sprint characters and basic night phase.

--- src/index.js
+++ src/index.js
@@ -7,36 +7,37 @@ angular.module('main', [material]).controller('mainController', [
     function () {
         var vm = this;
         vm.$onInit = function () {
-            vm.minions = [
-                "Mordred",
-                "Morgana",
-                "Oberon",
-                "Assassin",
-                "Minion of Mordred 1",
-                "Minion of Mordred 2",
-                "Minion of Mordred 3",
+            vm.dexter = [
+                "The Duke",
+                "Support Manager",
+                "Scrum Lord",
+                "Chicken Parm",
+                "IntelleWater",
+                "Level III Dev",
+                "Remote Dev I",
+                "Remote Dev II",
+                "Billy"
             ]
 
-            vm.servants = [
-                "Merlin",
-                "Percival",
-                "Loyal Servant of Arthur 1",
-                "Loyal Servant of Arthur 2",
-                "Loyal Servant of Arthur 3",
-                "Loyal Servant of Arthur 4",
-                "Loyal Servant of Arthur 5"
+            vm.sinister = [
+                "Sniper",
+                "Nerlin",
+                "Dev Slayer",
+                "Village Idiot",
+                "Intern",
+                "Level II Dev",
+                "Max Level Dev",
+                "Remote Dev",
+                "Billy"
             ]
             vm.steps = {
                 step_1: "Everyone close your eyes and extend your hand info a fist in front of you.",
-                step_2: "Minions of Mordred open your eyes and look around so that you know all agents of Evil.",
-                step_3: "Minions of Mordred close your eyes.",
-                step_4: "All players should have their eyes closed and hands in a fist in front of them.",
-                step_5: "Minions of Mordred - extend your thumb so that Merlin will know of you.",
-                step_6: "Merlin, open your eyes and see the agents of evil.",
-                step_7: "Minions of Mordred - put your thumbs down and re-form your hands into a fist.",
-                step_8: "Merlin, close your eyes.",
-                step_9: "All players have their eyes closed and hands in a fist in front of them.",
-                step_10: "Everyone open your eyes."
+                step_2: "Sinister Spies open your eyes, lift your thumbs, and look around to identify your fellow squad mates",
+                step_3: "Sinister Spies, close your eyes and lower your thumbs.",
+                step_4: "Sinister Spies, lift your thumbs.",
+                step_5: "The Duke, open your eyes and make note of the Sinister Spies. Use your knowledge carefully to prevent them from closing too many Pull Requests",
+                step_6: "Close your eyes and lower your thumbs.",
+                step_7: "Everyone wake up."
             }
         }
     }
